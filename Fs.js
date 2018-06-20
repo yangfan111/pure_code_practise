b3e5a572d60ad258d899501b72bd57d98c2606f6
yangfan01 <yangfan01@shinezone.com>
2018-06-20 14:29:14 +0800

    1

---
+++ Fs.js
@@ -0,0 +1,16 @@
+
+// const jsonComment = require("comment-json");
+// const fs = require("fs");
+// const PREFXI = '//';
+// let jsonObj = fs.readFileSync('F:/WildestDreams-Server/thrd/unthinkm/config.json')
+//  let fileObj = jsonComment.parse(jsonObj)
+// for(let name in fileObj)
+// {
+//     console.log("name:%s",name)
+//     console.info("object:%s",fileObj[name])
+// }
+let para 
+for(let i of para)
+{
+    console.log("111")
+}
